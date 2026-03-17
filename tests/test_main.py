1271ee4cd708bceca267c1f1a6bc2a0acf56e0ed
Bohdan3457 <bohdankhomenko04@gmail.com>
2026-03-17 20:47:02 +0100

    fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,19 @@
-
+import io
+from contextlib import redirect_stdout
 import pytest
+import inspect
 
 from app.main import calculate_team_total_rating, elves_concert, feast_of_the_dwarves
-from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.elves.druid import Druid
+from app.players.player import Player
 from app.players.elves.elf import Elf
 from app.players.elves.elf_ranger import ElfRanger
-from app.players.player import Player
+from app.players.elves.druid import Druid
+from app.players.dwarves.dwarf import Dwarf
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
 
 
+from app import main
 @pytest.mark.parametrize(
     "class_,methods",
     [
