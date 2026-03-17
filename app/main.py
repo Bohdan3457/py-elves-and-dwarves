28195aa2b34ac2f7eeb1f7fd71d6f6db1c9bf786
Bohdan3457 <bohdankhomenko04@gmail.com>
2026-03-17 21:36:20 +0100

    fix problem2

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ def elves_concert(elves: list) -> None:
     for elf in elves:
         elf.play_elf_song()
 
+
 def feast_of_the_dwarves(dwarves: list) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
